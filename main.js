cd05fee88aaba64e9688d135c14e5a30547b63d8
imali123 <imali62426@gmail.com>
2024-03-01 19:01:20 +0400

    responsive fix

--- main.js
+++ main.js
@@ -101,9 +101,6 @@
     var mySwiper = new Swiper('.home-swiper-container', {
       loop: false,
       speed: 1000,
-      // autoplay: {
-      //     delay: 3000,
-      // },
       effect: 'coverflow',
       grabCursor: true,
       centeredSlides: true,
@@ -115,14 +112,48 @@
           modifier: 1,
           slideShadows: false,
       },
-  
-      // Navigation arrows
       navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
       },
       initialSlide: 1,
-  });  
+      breakpoints: {
+          // when window width is <= 480px
+          480: {
+              slidesPerView: 1,
+              coverflowEffect: {
+                  rotate: 0,
+                  stretch: 50,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: false,
+              },
+          },
+          // when window width is <= 768px
+          768: {
+              slidesPerView: 2,
+              coverflowEffect: {
+                  rotate: 0,
+                  stretch: 50,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: false,
+              },
+          },
+          // when window width is <= 1300px
+          1300: {
+              slidesPerView: 3,
+              coverflowEffect: {
+                  rotate: 0,
+                  stretch: 80,
+                  depth: 200,
+                  modifier: 1,
+                  slideShadows: false,
+              },
+          },
+      },
+  });
+  
   
 
 
