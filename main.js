c03c2e446c44ea10cc4ac9c9debcea75f4dba21d
imali123 <imali62426@gmail.com>
2024-03-04 21:54:30 +0400

    companies

--- main.js
+++ main.js
@@ -99,11 +99,14 @@
       }
     });
     var mySwiper = new Swiper('.home-swiper-container', {
-      loop: false,
+      loop: true,
       speed: 1000,
+      autoplay: {
+        delay: 5000,
+    },
       effect: 'coverflow',
       grabCursor: true,
-      centeredSlides: true,
+      centeredSlides: false,
       slidesPerView: 'auto',
       coverflowEffect: {
           rotate: 0,
@@ -116,42 +119,42 @@
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
       },
-      initialSlide: 1,
-      breakpoints: {
-          // when window width is <= 480px
-          480: {
-              slidesPerView: 1,
-              coverflowEffect: {
-                  rotate: 0,
-                  stretch: 50,
-                  depth: 100,
-                  modifier: 1,
-                  slideShadows: false,
-              },
-          },
-          // when window width is <= 768px
-          768: {
-              slidesPerView: 2,
-              coverflowEffect: {
-                  rotate: 0,
-                  stretch: 50,
-                  depth: 100,
-                  modifier: 1,
-                  slideShadows: false,
-              },
-          },
-          // when window width is <= 1300px
-          1300: {
-              slidesPerView: 3,
-              coverflowEffect: {
-                  rotate: 0,
-                  stretch: 80,
-                  depth: 200,
-                  modifier: 1,
-                  slideShadows: false,
-              },
-          },
-      },
+      initialSlide: 2,
+      // breakpoints: {
+      //     // when window width is <= 480px
+      //     480: {
+      //         slidesPerView: 1,
+      //         coverflowEffect: {
+      //             rotate: 0,
+      //             stretch: 50,
+      //             depth: 100,
+      //             modifier: 1,
+      //             slideShadows: false,
+      //         },
+      //     },
+      //     // when window width is <= 768px
+      //     768: {
+      //         slidesPerView: 2,
+      //         coverflowEffect: {
+      //             rotate: 0,
+      //             stretch: 50,
+      //             depth: 100,
+      //             modifier: 1,
+      //             slideShadows: false,
+      //         },
+      //     },
+      //     // when window width is <= 1300px
+      //     1300: {
+      //         slidesPerView: 3,
+      //         coverflowEffect: {
+      //             rotate: 0,
+      //             stretch: 80,
+      //             depth: 200,
+      //             modifier: 1,
+      //             slideShadows: false,
+      //         },
+      //     },
+      // },
   });
   
   
@@ -207,7 +210,6 @@ function filterCompanies() {
           document.querySelector('.companys-list').innerHTML = response;
       },
       error: function () {
-          // Handle error, if any
           document.querySelector('.companys-list').innerHTML = '<p>Error loading companies.</p>';
       }
   });
